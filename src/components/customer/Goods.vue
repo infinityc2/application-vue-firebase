1dc504f7c739e1916b2bbb429ff92afe78927192
infinityc2 <super_star@hotmail.co.th>
2019-06-11 05:50:34 +0700

    goods dialog decorated

--- src/components/customer/Goods.vue
+++ src/components/customer/Goods.vue
@@ -7,7 +7,49 @@
         </v-toolbar-side-icon>
         <v-toolbar-title>รายการสินค้า</v-toolbar-title>
         <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn flat @click.stop="dialog = !dialog">
+            <v-icon>local_grocery_store</v-icon>สินค้าในตะกร้า
+          </v-btn>
+        </v-toolbar-items>
       </v-toolbar>
+      <v-dialog v-model="dialog" width="750px">
+        <v-card>
+          <v-toolbar color="green" dark>
+            <v-toolbar-title>สินค้าในตะกร้า</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn @click="dialog = false" color="red">
+              <v-icon>clear</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-container v-if="this.basket.length === 0">
+            <v-flex class="text-md-center">
+              <v-icon color="red">report</v-icon>
+              <h3 class="font-weight-medium">ไม่มีสินค้าในตะกร้า</h3>
+            </v-flex>
+          </v-container>
+          <v-list two-line v-else>
+            <template v-for="item in basket">
+              <v-subheader :key="item.id">
+                {{ item.name }}
+              </v-subheader>
+              <v-list-tile :key="item.id">
+                <v-list-tile-avatar>
+                  <v-img src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"></v-img>
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>ประเภท {{ item.type }}</v-list-tile-title>
+                  <v-list-tile-sub-title>ราคา {{ item.price }} บาท</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-btn flat @click="popBasket(item.id)">
+                  <v-icon>clear</v-icon>ลบ
+                </v-btn>
+              </v-list-tile>
+              <v-divider :key="item.id"></v-divider>
+            </template>
+          </v-list>
+        </v-card>
+      </v-dialog>
       <v-container>
         <v-card elevation="15">
           <v-card-title>รายการสินค้า</v-card-title>
@@ -31,7 +73,7 @@
                   <p class="body-2">ราคา {{ menu.price }} บาท</p>
                 </v-card-text>
                 <v-card-actions>
-                  <v-btn flat color="green" @click="pushBasket(menu, basket)">เลือก</v-btn>
+                  <v-btn flat color="green" @click="pushBasket(menu)">เลือก</v-btn>
                 </v-card-actions>
               </v-card>
             </v-layout>
@@ -44,21 +86,8 @@
 
 <script>
 import firebase from 'firebase'
-import Vue from 'vue'
-
-var basketDialog = Vue.component('basketDialog', {
-  props: ['dialog', 'basket'],
-  template: `
-    <v-dialog>
-      
-    </v-dialog>
-  `
-})
 
 export default {
-  components: {
-    basketDialog
-  },
   data () {
     return {
       menus: [],
@@ -71,12 +100,16 @@ export default {
     this.$store.dispatch('getMenu', this.menus)
   },
   methods: {
-    pushBasket (menu, basket) {
+    pushBasket (menu) {
       this.$store.dispatch('pushBasket', {
         menu: menu,
-        basket: basket
+        basket: this.basket
       })
       this.alert = true
+    },
+    popBasket (id) {
+      var index = this.basket.findIndex(item => item.id === id)
+      this.basket.splice(index, 1)
     }
   }
 }
